a7f4e1c41db5af35fcc2ecbb4b639afda783b79d
aristidei <a.isingizwe@alustudent.com>
2024-07-14 12:43:21 +0200

    C# kattas

---
+++ CatsAndDogs.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Saint
+{
+          public class Program
+          {
+                    public static void Main(string[] args)
+                    {
+                              Solve()
+                    }
+
+                    public static int Solve(List<char> xs, int n)
+                    {
+                              throw new NotImplementedException();
+                    }
+          }
+
+}
+
