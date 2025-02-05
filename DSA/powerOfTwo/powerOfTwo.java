87d226ed6b1df9a8427ee840d3bf78f6eb8587ee
aristidei <a.isingizwe@alustudent.com>
2025-02-05 11:51:39 +0200

    DSA

--- DSA/powerOfTwo/powerOfTwo.java
+++ DSA/powerOfTwo/powerOfTwo.java
@@ -1,17 +1,17 @@
 package DSA.powerOfTwo;
 
 public class powerOfTwo {
-          public static void main(String[] args){
+          public static void main(String[] args) {
                     int n = 5;
                     System.out.println(isPowerOfTwo(n));
           }
 
-          public static boolean isPowerOfTwo(int n){
-                    if(n <= 1){
+          public static boolean isPowerOfTwo(int n) {
+                    if (n <= 1) {
                               return false;
                     }
 
-                    while (n % 2 == 0){
+                    while (n % 2 == 0) {
                               n /= 2;
                     }
 
--- DSA/sorting/merge.js
+++ DSA/sorting/merge.js
@@ -1,4 +1,22 @@
-function merge(arr) {}
+function merge(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...merge(left), pivot, ...merge(right)];
+}
 
 console.log(merge([5, 3, 8, 2, 1, 50])); // [1, 2, 3, 4, 5, 8]
 console.log(merge([5, 3, 8, 2, 1])); // [1, 2, 3, 5, 8]
