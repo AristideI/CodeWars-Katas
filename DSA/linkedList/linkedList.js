137137d71424f3d840b92398e450eea6b0badb58
aristidei <a.isingizwe@alustudent.com>
2025-02-24 10:41:07 +0200

    linked list

---
+++ DSA/linkedList/linkedList.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (!this.isEmpty()) {
+      node.next = this.head;
+    }
+    this.head = node;
+    this.size++;
+  }
+
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  insert(value, index) {}
+}
+
+const linkedList = new LinkedList();
+linkedList.prepend(1);
+linkedList.prepend(2);
+linkedList.prepend(3);
+
+console.log(linkedList);
